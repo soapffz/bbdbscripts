c774c22c5dd5c1d0b3d7edbab95d2bca3baa8419
soapffz <43143519+soapffz@users.noreply.github.com>
2024-03-27 15:24:33 +0800

    Add files via upload

---
+++ debug_bbdb_new_subdomain_site_in_txt_to_bbdb.py
@@ -0,0 +1,259 @@
+"""
+new Env('根据bbdb已有根域名导入文本内容');
+* * * * * https://raw.githubusercontent.com/soapffz/bbdbscripts//main/bbdb_new_subdomain_site_in_txt_to_bbdb.py
+
+文件名: debug_bbdb_new_subdomain_site_in_txt_to_bbdb.py
+作者: soapffz
+创建日期: 2024年3月26日
+最后修改日期: 2024年3月27日
+
+在已有根域名的情况下，从文本文件解析尝试插入子域名和站点，支持ip格式的URL，注意一定要处理好文本文件
+
+"""
+
+import re
+from pymongo import MongoClient
+from datetime import datetime, timezone, timedelta
+from urllib.parse import urlparse
+
+# MongoDB连接信息
+mongo_uri = "mongodb://192.168.2.188:27017/"
+
+# 连接到MongoDB
+client = MongoClient(mongo_uri)
+db = client["bbdb"]
+
+
+def log_message(message, is_positive=True):
+    """打印日志信息"""
+    prefix = "[ + ]" if is_positive else "[ - ]"
+    print(
+        f"{datetime.now(timezone(timedelta(hours=8))).strftime('%Y-%m-%d %H:%M:%S')} {prefix} {message}"
+    )
+
+
+def preprocess_lines(lines):
+    processed_lines = []
+    for line in lines:
+        line = line.strip()
+        if line:
+            # 去除以*.开头的域名
+            line = re.sub(r"^\*\.", "", line)
+            # 处理如果开头直接是*加上域名的情况，去除*
+            line = re.sub(r"^\*", "", line)
+            processed_lines.append(line)
+    return processed_lines
+
+
+def preprocess_url(url):
+    # 使用正则表达式匹配URL
+    url_match = re.match(r"^(https?://)?([\w\.-]+?)(:\d+)?(/.*)?$", url)
+    if url_match:
+        scheme = url_match.group(1)
+        hostname = url_match.group(2)
+        port = url_match.group(3) or ""
+        path = url_match.group(4) or ""
+        # 重构URL，确保其结构正确
+        processed_url = f"{scheme}{hostname}{port}{path}"
+        return processed_url, scheme, hostname
+    else:
+        # 如果URL格式不正确，返回None和空字符串作为scheme
+        return None, "", ""
+
+
+def parse_and_process(lines):
+    root_domains = list(db.root_domain.find())
+    root_domains_set = {doc["name"] for doc in root_domains}
+    sub_domains_to_insert = []
+    sites_to_insert = []
+    ips_to_insert = []
+
+    blacklist_sub_domains = set(
+        doc["name"] for doc in db.blacklist.find({"type": "sub_domain"})
+    )
+    blacklist_urls = set(doc["name"] for doc in db.blacklist.find({"type": "url"}))
+    blacklist_ips = set(doc["name"] for doc in db.blacklist.find({"type": "ip"}))
+    existing_root_domains = set(doc["name"] for doc in db.root_domain.find())
+    existing_sub_domains = set(doc["name"] for doc in db.sub_domain.find())
+    existing_sites = set(doc["name"] for doc in db.site.find())
+    log_message("加载数据库完成")
+
+    lines = preprocess_lines(lines)
+    log_message("文本文件读取完成")
+
+    # 解析根域名
+    for line in lines:
+        if line.startswith("http"):
+            continue
+        root_domain_match = re.search(r"^(.*?\.\w+)\.\w+$", line)
+        if root_domain_match:
+            root_domain = root_domain_match.group()
+            if (
+                root_domain in root_domains_set
+                and root_domain not in blacklist_sub_domains
+            ):
+                root_domain_obj = next(
+                    (doc for doc in root_domains if doc["name"] == root_domain), None
+                )
+                root_domain_id = str(root_domain_obj["_id"])
+                business_id = str(root_domain_obj["business_id"])
+
+                # 解析子域名
+                sub_domain = line.split(root_domain)[0].rstrip(".")
+                if (
+                    sub_domain != root_domain
+                    and sub_domain not in blacklist_sub_domains
+                    and sub_domain not in existing_sub_domains
+                    and sub_domain != ""
+                ):
+                    sub_domain_data = {
+                        "name": sub_domain,
+                        "notes": "set by soapffz with script",
+                        "root_domain_id": root_domain_id,
+                        "business_id": business_id,
+                        "create_time": datetime.utcnow(),
+                        "update_time": datetime.utcnow(),
+                    }
+                    sub_domains_to_insert.append(sub_domain_data)
+
+    # 加载子域名数据
+    sub_domains = list(db.sub_domain.find())
+
+    # 解析站点信息
+    for line in lines:
+        # 匹配 HTTP/HTTPS 协议头或纯域名
+        processed_url, scheme, hostname = preprocess_url(line)
+        if processed_url and scheme:
+            if scheme.startswith("http://") or scheme.startswith("https://"):
+                # 处理 IP 地址形式的 URL
+                if re.match(r"\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$", hostname):
+                    ip_address = hostname
+                    if ip_address not in blacklist_ips:
+                        ip_docs = list(db.ip.find({"address": ip_address}))
+                        if ip_docs:
+                            sub_domain_id = None
+                            root_domain_id = None
+                            business_id = None
+                            for ip_doc in ip_docs:
+                                for entry in ip_doc.get("sub_domains", []):
+                                    if "sub_domain_id" in entry:
+                                        sub_domain_id = str(entry["sub_domain_id"])
+                                        root_domain_id = str(entry["root_domain_id"])
+                                        business_id = str(entry["business_id"])
+                                        break
+                                if sub_domain_id:
+                                    break
+                            if not sub_domain_id:
+                                for ip_doc in ip_docs:
+                                    for entry in ip_doc.get("root_domains", []):
+                                        if "root_domain_id" in entry:
+                                            root_domain_id = str(
+                                                entry["root_domain_id"]
+                                            )
+                                            business_id = str(entry["business_id"])
+                                            break
+                                    if root_domain_id:
+                                        break
+                            hostname = (
+                                db.sub_domain.find_one({"_id": sub_domain_id})["name"]
+                                if sub_domain_id
+                                else db.root_domain.find_one({"_id": root_domain_id})[
+                                    "name"
+                                ]
+                            )
+                            if (
+                                processed_url not in blacklist_urls
+                                and processed_url not in existing_sites
+                            ):
+                                site_data = {
+                                    "name": processed_url,
+                                    "status": "",
+                                    "title": "",
+                                    "hostname": hostname,
+                                    "ip": ip_address,
+                                    "http_server": "",
+                                    "body_length": "",
+                                    "headers": "",
+                                    "keywords": "",
+                                    "applications": [],
+                                    "applications_categories": [],
+                                    "applications_types": [],
+                                    "applications_levels": [],
+                                    "application_manufacturer": [],
+                                    "fingerprint": [],
+                                    "root_domain_id": root_domain_id,
+                                    "sub_domain_id": sub_domain_id,
+                                    "business_id": business_id,
+                                    "notes": "set by soapffz with script",
+                                    "create_time": datetime.now(
+                                        timezone(timedelta(hours=8))
+                                    ),
+                                    "update_time": datetime.now(
+                                        timezone(timedelta(hours=8))
+                                    ),
+                                }
+                                sites_to_insert.append(site_data)
+
+                # 处理域名形式的 URL
+                else:
+                    for sub_domain in sub_domains:
+                        if hostname.endswith(sub_domain["name"]):
+                            sub_domain_id = str(sub_domain["_id"])
+                            root_domain_id = sub_domain["root_domain_id"]
+                            business_id = sub_domain["business_id"]
+                            if (
+                                processed_url not in blacklist_urls
+                                and processed_url not in existing_sites
+                            ):
+                                site_data = {
+                                    "name": processed_url,
+                                    "status": "",
+                                    "title": "",
+                                    "hostname": hostname,
+                                    "ip": "",
+                                    "http_server": "",
+                                    "body_length": "",
+                                    "headers": "",
+                                    "keywords": "",
+                                    "applications": [],
+                                    "applications_categories": [],
+                                    "applications_types": [],
+                                    "applications_levels": [],
+                                    "application_manufacturer": [],
+                                    "fingerprint": [],
+                                    "root_domain_id": root_domain_id,
+                                    "sub_domain_id": sub_domain_id,
+                                    "business_id": business_id,
+                                    "notes": "set by soapffz with script",
+                                    "create_time": datetime.now(
+                                        timezone(timedelta(hours=8))
+                                    ),
+                                    "update_time": datetime.now(
+                                        timezone(timedelta(hours=8))
+                                    ),
+                                }
+                                sites_to_insert.append(site_data)
+                                break
+
+    # 批量插入数据
+    if sub_domains_to_insert:
+        db.sub_domain.insert_many(sub_domains_to_insert)
+        # log_message(sub_domains_to_insert)
+        log_message(f"Inserted {len(sub_domains_to_insert)} sub domains.")
+    if sites_to_insert:
+        db.site.insert_many(sites_to_insert)
+        # log_message(sites_to_insert)
+        log_message(f"Inserted {len(sites_to_insert)} sites.")
+
+
+def main():
+    log_message("开始解析文件...")
+    with open("domain.txt", "r") as file:
+        lines = file.readlines()
+
+    parse_and_process(lines)
+    log_message("解析完成!")
+
+
+if __name__ == "__main__":
+    main()
